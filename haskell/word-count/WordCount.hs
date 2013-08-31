cf6eb00496388295513c64e894e0695d62ee9091
Gavin Stark <gavin@gstark.com>
2013-08-31 15:34:20 -0400

    Uses fromListWith

--- haskell/word-count/WordCount.hs
+++ haskell/word-count/WordCount.hs
@@ -1,17 +1,8 @@
 module WordCount where
 import Data.Char
 import Data.List
-import Data.Map (fromList)
+import Data.Map (fromListWith)
+import Data.List.Split
 
--- Is there a better way to implement the onlyAlphaNum than a lambda containing an if?
--- Originally I had:
---  where onlyAlphaNum x
---         | isAlpha x || isDigit x = toLower x
---         | otherwise = ' '
---
--- But I'm not sure which is more idiomatic Haskell
-
-wordCount = (fromList . map toCount . group . sort . words . map onlyAlphaNum . map toLower)
-  where onlyAlphaNum = (\x -> if isAlphaNum x then x else ' ')
-        toCount      = (\x -> (head x, length x))
+wordCount = fromListWith (+) . map (\x -> (x,1)) . wordsBy (not . isAlphaNum) . map toLower
 
